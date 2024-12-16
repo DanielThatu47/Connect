354870c02e205b2ab6433cff7eef502521422ee0
DanielThatu47 <141019551+DanielThatu47@users.noreply.github.com>
2024-12-16 23:55:35 +0530

    Update settings.py

--- connect/settings.py
+++ connect/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d5yk&5o5e^kj!cuz*=jh6%3!tj!9^&e#)1@7%+&c(0j9q&e8-t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.44.239', '0.0.0.0' ,'127.0.0.1' ,'*']
 
