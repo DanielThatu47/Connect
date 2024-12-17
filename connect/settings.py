28d70646411f0355a812974c880fbb58e9206c30
DanielThatu47 <141019551+DanielThatu47@users.noreply.github.com>
2024-12-17 11:52:43 +0530

    Update settings.py

--- connect/settings.py
+++ connect/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
    'django_extensions',
     'Application',
     
@@ -249,11 +248,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "/media/","/static/")
-]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
