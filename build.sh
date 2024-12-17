20721015cf277e047b4f8d9ea2963ceb352c391d
DanielThatu47 <141019551+DanielThatu47@users.noreply.github.com>
2024-12-17 12:28:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,6 @@
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
 
-# Collect static files (for production deployment)
-python manage.py collectstatic --noinput
 
-python manage.py create_admin.py
 # Run database migrations
 python manage.py migrate --noinput
