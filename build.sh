08518175bff97fe23045199a8305f804ebc8cf46
DanielThatu47 <141019551+DanielThatu47@users.noreply.github.com>
2025-03-05 09:05:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Install dependencies from requirements.txt
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 
 # Run database migrations
-python manage.py migrate --noinput
+python3 manage.py migrate --noinput
 
 
